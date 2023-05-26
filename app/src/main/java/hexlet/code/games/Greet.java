10c220a77ba289e55df957c8d82f28c49d9725af
Aleksandr Gaponchikov <aleksandrgaponcikov@gmail.com>
2023-05-26 09:44:09 +0500

    fix 'i' -> 'I'

--- app/src/main/java/hexlet/code/games/Greet.java
+++ app/src/main/java/hexlet/code/games/Greet.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Greet {
     public static String greetPlayer(Scanner scannerIn) {
         System.out.println("Welcome to the Brain Games!");
-        System.out.println("May i have your name?");
+        System.out.println("May I have your name?");
         String playerName = scannerIn.next();
         System.out.printf("Hello, %s!%n", playerName);
 
