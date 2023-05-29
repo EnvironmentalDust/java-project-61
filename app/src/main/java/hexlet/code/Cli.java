4101e594174f68d836702a029456531515111adb
Aleksandr Gaponchikov <aleksandrgaponcikov@gmail.com>
2023-05-30 00:19:45 +0500

    fix 'i' -> 'I'

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -6,7 +6,7 @@ public class Cli {
 
     public static String welcomePlayer() {
         System.out.println("Welcome to the Brain Games!");
-        System.out.println("May i have your name?");
+        System.out.println("May I have your name?");
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
