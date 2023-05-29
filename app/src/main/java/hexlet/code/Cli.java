c77317cc3707048bc79e22f13cbb2d791fdc52f3
Aleksandr Gaponchikov <aleksandrgaponcikov@gmail.com>
2023-05-30 00:16:22 +0500

    fix missing line

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Cli {
 
     public static String welcomePlayer() {
+        System.out.println("Welcome to the Brain Games!");
         System.out.println("May i have your name?");
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.next();
